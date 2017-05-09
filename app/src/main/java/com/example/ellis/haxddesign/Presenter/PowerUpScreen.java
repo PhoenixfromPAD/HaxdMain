4494aa094cf445c145efe9489b3b76655fd83ffa
Ellis <espellerberg@gmail.com>
2017-05-09 13:36:32 -0700

    Power Up Screen Updates

--- app/src/main/java/com/example/ellis/haxddesign/Presenter/PowerUpScreen.java
+++ app/src/main/java/com/example/ellis/haxddesign/Presenter/PowerUpScreen.java
@@ -2,14 +2,39 @@ package com.example.ellis.haxddesign.Presenter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ellis.haxddesign.R;
 
 public class PowerUpScreen extends AppCompatActivity {
+    private Button pauseUse, terminateUse, locateUse, slowUse, buttonback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_power_up_screen);
+        wireWidgets();
+
+        pauseUse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //pause on use
+            }
+        });
+        terminateUse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //terminate on use
+            }
+        });
+    }
+
+    private void wireWidgets() {
+        pauseUse = (Button) findViewById(R.id.buttonPauseUse);
+        terminateUse = (Button) findViewById(R.id.buttonTerminateUse);
+        locateUse = (Button) findViewById(R.id.buttonLocateUse);
+        slowUse = (Button) findViewById(R.id.buttonSlowUse);
+        buttonback = (Button) findViewById(R.id.buttonBack);
     }
 }
